4372db650e7ea547cdf8bec973abcd021c2d7b9c
Lina <lina.vandre@uni-siegen.de>
2023-04-13 16:58:11 +0200

    playing with buffer space

--- compare_distributions.py
+++ compare_distributions.py
@@ -11,18 +11,27 @@ Goal of this program:
 
 """simulation parameters"""
 #memory_numbers = [1,1,1,1,1,1,1,1]
-generation_probability=0.01
-#swapping_probability=1
-number_of_repetitions=100  
+#generation_probability=0.01
+swapping_probability=0.5
+number_of_repetitions=1000  
 
+const=1000
 memory_numbers_list = [
-    [1,1,1,1,1,1,1,1],
+    #[1,1,1,1,1,1,1,1],
     #[2,1,1,2,2,1,1,2],
     #[3,1,1,1,1,1,1,3],
     #[2,1,2,1,2,1,2,1],
     #[5,5,5,5,5,5,5,5],
-    [10,10,10,10,10,10,10,10],
-    [100,100,100,100,100,100,100,100]
+    #[10,10,10,10,10,10,10,10],
+    #[100,100,100,100,100,100,100,100]
+    [const,1,1,const,const,1,1,const],
+    [const,2,2,const,const,2,2,const],
+    [const,3,3,const,const,3,3,const],
+    [const,4,4,const,const,4,4,const],
+    [const,5,5,const,const,5,5,const],
+    #[const,6,6,const,const,6,6,const],
+    #[const,7,7,const,const,7,7,const],
+    #[const,8,8,const,const,8,8,const],
     ]
 
 steps = 9
@@ -49,7 +58,8 @@ for j in range(len(memory_numbers_list)):
     """varry swapping_probability"""
     for i in range(0,steps+1):
         #print(i) 
-        swapping_probability=amin + i*stepsize
+        #swapping_probability=amin + i*stepsize
+        generation_probability =amin + i*stepsize
         #print(swapping_probability)
         data = memb.get_statistics(memory_numbers = memory_numbers,
                           generation_probability=generation_probability,
@@ -68,9 +78,11 @@ for j in range(len(memory_numbers_list)):
     plt.errorbar(dataa,datamean_list[j],datastdev_list[j],
              fmt ='x',capsize=3,label=r'%s' %(memory_numbers_list[j]))
 
-plt.xlabel('swapping probability $a$')
+#plt.xlabel('swapping probability $a$')
+plt.xlabel('generation probability $p$')
 plt.ylabel(r'waiting time')
-plt.title('generation probability=%f, number_of_repetitions=%i' %(generation_probability,number_of_repetitions))
+#plt.title('generation probability=%f, number_of_repetitions=%i' %(generation_probability,number_of_repetitions))
+plt.title('swapping probability=%f, number_of_repetitions=%i' %(swapping_probability,number_of_repetitions))
 
 
 plt.legend()
