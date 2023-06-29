fc71c7368847fc3844030cd9c1e518412f1388b7
Lina <lina.vandre@uni-siegen.de>
2023-06-29 15:15:36 +0800

    new script with contour map for a and p

--- compare_distributions.py
+++ compare_distributions.py
@@ -10,29 +10,31 @@ Goal of this program:
 """
 
 """simulation parameters"""
-#memory_numbers = [1,1,1,1,1,1,1,1]
+memory_numbers = [2,1,1,2,2,1,1,2]
 #generation_probability=0.01
 swapping_probability=0.5
-number_of_repetitions=1000  
+number_of_repetitions=300 
 
 const=1000
 memory_numbers_list = [
     #[1,1,1,1,1,1,1,1],
-    #[2,1,1,2,2,1,1,2],
-    #[3,1,1,1,1,1,1,3],
+    [2,1,1,2,2,1,1,2],
+    [3,1,1,1,1,1,1,3],
     #[2,1,2,1,2,1,2,1],
     #[5,5,5,5,5,5,5,5],
     #[10,10,10,10,10,10,10,10],
     #[100,100,100,100,100,100,100,100]
-    [const,1,1,const,const,1,1,const],
-    [const,2,2,const,const,2,2,const],
-    [const,3,3,const,const,3,3,const],
-    [const,4,4,const,const,4,4,const],
-    [const,5,5,const,const,5,5,const],
+    #[const,1,1,const,const,1,1,const],
+    #[const,2,2,const,const,2,2,const],
+    #[const,3,3,const,const,3,3,const],
+    #[const,4,4,const,const,4,4,const],
+    #[const,5,5,const,const,5,5,const],
     #[const,6,6,const,const,6,6,const],
     #[const,7,7,const,const,7,7,const],
     #[const,8,8,const,const,8,8,const],
     ]
+
+
 
 steps = 9
 amin = 0.1
@@ -42,27 +44,25 @@ stepsize =  (amax-amin)/steps
 dataa = []
 datamean_list = []
 datastdev_list = []
-
-
-dataa = [amin + i*stepsize for i in range(0,steps+1)]
-#print(dataa)
+dataa = np.linspace(0.1,1,10)
 
 """vary distribution of memory_numbers"""
 
 for j in range(len(memory_numbers_list)):
     memory_numbers = memory_numbers_list[j]
     #print(memory_numbers)
+
     datamean = []
     datastdev = []
 
     """varry swapping_probability"""
-    for i in range(0,steps+1):
+    for gen_prob in dataa:
         #print(i) 
         #swapping_probability=amin + i*stepsize
-        generation_probability =amin + i*stepsize
+        #generation_probability =amin + i*stepsize
         #print(swapping_probability)
         data = memb.get_statistics(memory_numbers = memory_numbers,
-                          generation_probability=generation_probability,
+                          generation_probability=gen_prob,
                           swapping_probability=swapping_probability,
                           number_of_repetitions=number_of_repetitions)
         #print(data)
@@ -78,11 +78,14 @@ for j in range(len(memory_numbers_list)):
     plt.errorbar(dataa,datamean_list[j],datastdev_list[j],
              fmt ='x',capsize=3,label=r'%s' %(memory_numbers_list[j]))
 
+
+
 #plt.xlabel('swapping probability $a$')
 plt.xlabel('generation probability $p$')
 plt.ylabel(r'waiting time')
 #plt.title('generation probability=%f, number_of_repetitions=%i' %(generation_probability,number_of_repetitions))
 plt.title('swapping probability=%f, number_of_repetitions=%i' %(swapping_probability,number_of_repetitions))
+plt.yscale('log')
 
 
 plt.legend()
