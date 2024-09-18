e26ad3403b4edd895e221fef85862163df2d59ae
Lina <lina.vandre@uni-siegen.de>
2024-09-18 18:49:49 +0200

    more files prepared to collect data of plotts

--- Plots/compare_fixed_length.py
+++ Plots/compare_fixed_length.py
@@ -7,41 +7,45 @@ import csv
 """
 Goal of this program: 
 - computes waiting times for different swapping_probabilities and distributions of memory_numbers.
-- make plots: waiting_time(swapping_probability) for different memory distributions
+- all chains have the same length but some have more repeater stations in between. This implies a shorter distance and therefore the generation probability is larger
 """
 
 """simulation parameters"""
 #memory_numbers = [2,1,1,2,2,1,1,2]
 #generation_probability=0.01
-swapping_probability = 0.5
+#swapping_probability = 0.5
 number_of_repetitions_original=2000 
 element_numbers = 4
 
-const = 8
-bfix = [7,6,5,4]
-afix = [const - b for b in bfix]
+length_vec = [100, 50, 25, 12.5]
+afix = [8,4,2,1]
 
-file_name_list = ["compare_fixed_buffers_8-7_a5.csv", "compare_fixed_buffers_8-6_a5.csv", "compare_fixed_buffers_8-5_a5.csv", "compare_fixed_buffers_8-4_a5.csv"] ### file names: compare_fixed_buffers_const-bfix_.csv
+file_name_list = ["compare_fixed_length_32-2.csv", "compare_fixed_buffers_32-4.csv", "compare_fixed_buffers_32-8.csv", "compare_fixed_buffers_32-16.csv"] ### file names: compare_fixed_buffers_const-bfix_.csv
+
+def prob_function(length):
+    Latt = 22
+    return np.exp(-length/Latt)   
 
 
-sw_prob = swapping_probability
-datap = np.linspace(0.005,0.015,51)
+
+dataa = np.linspace(0.1,1,46)
 
 """vary distribution of memory_numbers"""
 
-for j in range(len(bfix)):
-    b = bfix[j]
+for j in range(len(afix)):
     a = afix[j]
-    memory_numbers = [b,a,a,b,b,a,a,b]
+    memory_numbers = [a,a,a,a,a,a,a,a]
     file_name = file_name_list[j]
     #print(memory_numbers)
+    gen_prob = prob_function(length_vec[j])
+    print(gen_prob)
     
     number_of_repetitions = number_of_repetitions_original
 
 
 
     """varry swapping_probability"""
-    for gen_prob in reversed(datap):
+    for sw_prob in reversed(dataa):
         #print(i) 
         #swapping_probability=amin + i*stepsize
         #generation_probability =amin + i*stepsize
@@ -67,7 +71,7 @@ for j in range(len(bfix)):
         "Daten in csv schreiben"
         with open(file_name, mode='a') as csvfile:
             csvfile_writer = csv.writer(csvfile, delimiter=',')
-            csvfile_writer.writerow([element_numbers, gen_prob, sw_prob, data[0], data[1], number_of_repetitions, a, b, data[1]/data[0]])
+            csvfile_writer.writerow([element_numbers, gen_prob, sw_prob, data[0], data[1], number_of_repetitions, a, data[1]/data[0]])
 
 
 
