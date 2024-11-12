cfcee6c99a60d1bcbed73fb79c1f9df5a678d42d
Lina <lina.vandre@uni-siegen.de>
2024-11-12 11:18:06 +0100

    new job

--- Plots/saturation_parameters.py
+++ Plots/saturation_parameters.py
@@ -19,11 +19,11 @@ Goal of this program:
 #datap = [0.1] # np.linspace(0.05,0.1,51)
 #sw_prob_list = [0.5] #np.linspace(0.1,1,91)
 
-file_name = "saturation_parameters_p05-1.csv"
+file_name = "saturation_parameters_diff001_p05-1_diff1.csv"
 element_numbers = 4
 
 
-with open('infty_buffer2.csv','r') as csvfile:
+with open('infty_buffer2_p05-1.csv','r') as csvfile:
     plots=csv.reader(csvfile, delimiter=',')
     #next(plots) #damit die erste zeile nicht eingelesen werden
     for row in plots:
@@ -35,8 +35,8 @@ with open('infty_buffer2.csv','r') as csvfile:
         
         #print(gen_prob, sw_prob, mean_inf, error_inf, number_of_repetitions)
         
-        mean_previous = mean_inf
-        error_previous = error_inf 
+        #mean_previous = mean_inf
+        #error_previous = error_inf 
 
     
         a = 1
@@ -53,15 +53,15 @@ with open('infty_buffer2.csv','r') as csvfile:
                          
         
         
-        while abs(data[0] - mean_previous) >  error_previous:
+        while abs(data[0] - mean_inf) >  0.01*mean_inf:
             #print("Differenz vorher: ", abs(mean_previous - data[0]), error_previous)
             a += 1
             #print(a)
             memory_numbers = [b,a,a,b,b,a,a,b] 
             #print(memory_numbers)
             
-            mean_previous = data[0]
-            error_previous = data[1]
+            #mean_previous = data[0]
+            #error_previous = data[1]
             
             data = memb.get_statistics(memory_numbers = memory_numbers,
                           generation_probability=gen_prob,
@@ -81,18 +81,18 @@ with open('infty_buffer2.csv','r') as csvfile:
                           swapping_probability=sw_prob,
                           number_of_repetitions=number_of_repetitions)
                           
-        mean_previous = mean_inf
-        error_previous = error_inf                  
+        #mean_previous = mean_inf
+        #error_previous = error_inf                  
                           
-        while abs(data[0] - mean_previous) >  error_previous:
+        while abs(data[0] - mean_inf) >  0.01*mean_inf:
             #print("Differenz vorher: ", abs(mean_previous - data[0]), error_previous)
             b += 1
             #print(b)
             memory_numbers = [b,a,a,b,b,a,a,b] 
             #print(memory_numbers)
             
-            mean_previous = data[0]
-            error_previous = data[1]    
+            #mean_previous = data[0]
+            #error_previous = data[1]    
             
             data = memb.get_statistics(memory_numbers = memory_numbers,
                           generation_probability=gen_prob,
